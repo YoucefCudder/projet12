64b8afe171f798015853610f7d172832aa6b3575
Youcef Aouali <you.aouali@gmail.com>
2023-01-13 13:27:58 +0100

    voir si finalisation ok

--- EpicEvents/AppEpic/views.py
+++ EpicEvents/AppEpic/views.py
@@ -2,7 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from AppEpic.models import Client, Contract, Event
+from AppEpic.models import Client, Event
 from AppEpic.permissions import ClientPermission, ContractPermission, EventPermission
 from AppEpic.serializer import ContractSerializer, EventSerializer, ClientSerializer
 
@@ -14,9 +14,7 @@ class ClientViewSet(ModelViewSet):
 
     filterset_fields = ['firstname', 'lastname', 'email']
 
-
     def perform_create(self, serializer):
-        # if self.request.user.groups.filter(name="SALES").exists():
         return serializer.save(sales_contact=self.request.user)
 
     def get_queryset(self, *args, **kwargs):
@@ -63,12 +61,13 @@ class EventViewSet(ModelViewSet):
         "^contract__client__email",
         "^event_date",
     ]
+
     def get_queryset(self, *args, **kwargs):
         if self.request.user.groups.filter(name="SALES").exists():
-            return Client.objects.filter(sales_contact=self.request.user)
+            return Event.objects.filter(client__sales_contact=self.request.user) # contract__sales_contact ?
         if self.request.user.groups.filter(name="SUPPORT").exists():
             event_id = [event.client.id for event in Event.objects.filter(support_contact=self.request.user)
-                         ]
+                        ]
             return Event.objects.filter(client__in=event_id)
 
     def perform_create(self, serializer):
